9540e87d4e3ee4ca2c16cb90bc4d5fa072e7f4e2
mfaizfatah <mfaizfatah@gmail.com>
2021-02-07 20:37:19 +0700

    change url

--- app/models/user.go
+++ app/models/user.go
@@ -2,14 +2,15 @@ package models
 
 // User is model for user data
 type User struct {
-	ID          int         `json:"id_user" gorm:"column:id"`
-	Username    string      `json:"username" gorm:"column:username"`
-	Email       string      `json:"email" gorm:"column:email"`
-	Password    string      `json:"password" gorm:"column:password"`
-	Name        string      `json:"name" gorm:"column:name"`
-	Telp        string      `json:"telp" gorm:"column:telp"`
-	DateOfBirth interface{} `json:"dateOfBirth" gorm:"column:date_of_birth"`
-	IDRole      int         `json:"idRole" gorm:"column:id_role"`
+	ID                int         `json:"id_user" gorm:"column:id"`
+	Username          string      `json:"username" gorm:"column:username"`
+	Email             string      `json:"email" gorm:"column:email"`
+	Password          string      `json:"password" gorm:"column:password"`
+	Name              string      `json:"name" gorm:"column:name"`
+	Telp              string      `json:"telp" gorm:"column:telp"`
+	DateOfBirth       interface{} `json:"dateOfBirth" gorm:"column:date_of_birth"`
+	IDRole            int         `json:"idRole" gorm:"column:id_role"`
+	EmailVerification int         `json:"emailVerify" gorm:"column:email_verify"`
 }
 
 // ForgotPass is mode for request forgot pass

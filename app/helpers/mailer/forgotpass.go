af118a092cd3e010492596070ac49c167362a203
mfaizfatah <mfaizfatah@gmail.com>
2021-02-22 19:19:09 +0700

    change forgot pass

--- app/helpers/mailer/forgotpass.go
+++ app/helpers/mailer/forgotpass.go
@@ -8,7 +8,7 @@ const (
 	// ForgotPasswordContent ...
 	ForgotPasswordContent = `Anda telah mengajukan permintaan untuk merubah kata sandi pada akun Story Tales anda. Dengan alasan keamanan, tombol ubah kata sandi di bawah hanya berlaku selama lima belas menit. Silahkan klik tombol di bawah untuk merubah kata sandi akun Story Tales anda.`
 	// ForgotPasswordFooter ...
-	ForgotPasswordFooter = `Harap abaikan email ini bila anda tidak pernah mengajukan permintaan perubahan kata sandi akun Story Tales.`
+	ForgotPasswordFooter = `Harap segera ganti password kamu atau abaikan email ini bila anda tidak pernah mengajukan permintaan perubahan kata sandi akun Story Tales.`
 	// ForgotPasswordLabel ...
 	ForgotPasswordLabel = `Ubah Kata Sandi`
 )
@@ -19,12 +19,12 @@ func ForgotPasswordForeword(nama string) string {
 }
 
 // NewforgotPassword ...
-func NewforgotPassword(name, link string) Template {
+func NewforgotPassword(name, newPass string) Template {
 	return &template{
 		title:       ForgotPasswordTitle,
 		foreword:    ForgotPasswordForeword(name),
 		content:     ForgotPasswordContent,
-		buttonLink:  link,
+		message:     newPass,
 		buttonLabel: ForgotPasswordLabel,
 		footer:      ForgotPasswordFooter,
 	}
